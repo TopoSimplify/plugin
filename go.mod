138610b69bc16c89a3d4a2f50c78ccaddf97938c
Titus Tienaah <titus.intdxdt@gmail.com>
2021-09-22 11:48:05 +0000

    added reading constraints

--- go.mod
+++ go.mod
@@ -4,10 +4,11 @@ go 1.16
 
 require (
 	github.com/TopoSimplify/constdp v1.0.0
+	github.com/TopoSimplify/geometry v0.0.2 // indirect
 	github.com/TopoSimplify/offset v0.0.0-20191215171217-3e0ac486281e
 	github.com/TopoSimplify/opts v1.0.0
 	github.com/intdxdt/fileutil v0.0.1
-	github.com/intdxdt/geom v1.0.1
+	github.com/intdxdt/geom v1.0.2
 	github.com/intdxdt/iter v1.0.0
 	github.com/intdxdt/math v1.0.0
 	github.com/naoina/go-stringutil v0.1.0 // indirect
--- read/main.go
+++ read/main.go
@@ -1,35 +1,182 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"github.com/TopoSimplify/geometry"
 	"github.com/intdxdt/fileutil"
+	"github.com/intdxdt/geom"
 	geojson "github.com/paulmach/go.geojson"
 	"strings"
 )
 
 func main() {
-	readJsonFile("input.json")
-	//fc2 := geojson.NewFeature()
-
+	var polyline = readInputPolylines("input.json")
+	var constraints = readInputConstraints("constraints.json")
+	fmt.Println(polyline)
+	fmt.Println(constraints)
 }
 
-func parseFeatures(inputs []string){
-	for _, fjson  := range inputs {
+func readInputPolylines(inputJsonFile string) []geometry.Polyline {
+	return parseInputLinearFeatures(readJsonFile(inputJsonFile))
+}
+
+func readInputConstraints(inputJsonFile string) geometry.GeoJSONGeometries  {
+	return parseConstraintFeatures(readJsonFile(inputJsonFile))
+}
+
+type JSONLineString struct {
+	Id          string
+	Coordinates [][]float64
+	Meta        string
+}
+
+type JSONPoint struct {
+	Id          string
+	Coordinates []float64
+	Meta        string
+}
+
+type JSONPolygon struct {
+	Id          string
+	Coordinates [][][]float64
+	Meta        string
+}
+
+func parseInputLinearFeatures(inputs []string) []geometry.Polyline {
+	var plns = make([]geometry.Polyline, 0, len(inputs))
+	for idx, fjson := range inputs {
 		feat, err := geojson.UnmarshalFeature([]byte(fjson))
 		checkError(err)
-		println(feat.Geometry.IsLineString() || feat.Geometry.IsMultiLineString())
+		var objs = getLineStringObjects(idx, feat)
+		for _, o := range objs {
+			var pln = createPolyline(o)
+			plns = append(plns, pln)
+		}
+	}
+	return plns
+}
+
+func parseConstraintFeatures(inputs []string) geometry.GeoJSONGeometries {
+	var pts = make([]geometry.Point, 0, len(inputs))
+	var plns = make([]geometry.Polyline, 0, len(inputs))
+	var polys = make([]geometry.Polygon, 0, len(inputs))
+
+	for idx, fjson := range inputs {
+		feat, err := geojson.UnmarshalFeature([]byte(fjson))
+		checkError(err)
+
+		var ptObjs = getPointObjects(idx, feat)
+		for _, o := range ptObjs {
+			pts = append(pts, createPoint(o))
+		}
+
+		var lnObjs = getLineStringObjects(idx, feat)
+		for _, o := range lnObjs {
+			plns = append(plns, createPolyline(o))
+		}
+
+		var plyObjs = getPolygonObjects(idx, feat)
+		for _, o := range plyObjs {
+			polys = append(polys, createPolygon(o))
+		}
+	}
+
+	return geometry.GeoJSONGeometries{
+		Points:      pts,
+		LineStrings: plns,
+		Polygons:    polys,
 	}
 }
 
+func createPoint(jsonLine JSONPoint) geometry.Point {
+	return geometry.CreatePoint(jsonLine.Id, jsonLine.Coordinates, jsonLine.Meta)
+}
 
+func createPolyline(jsonLine JSONLineString) geometry.Polyline {
+	var coords = geom.AsCoordinates(jsonLine.Coordinates)
+	return geometry.CreatePolyline(jsonLine.Id, coords, jsonLine.Meta)
+}
 
-func readJsonFile(file string) {
+func createPolygon(jsonLine JSONPolygon) geometry.Polygon {
+	var coords = make([]geom.Coords, 0, len(jsonLine.Coordinates))
+	for _, array := range jsonLine.Coordinates {
+		coords = append(coords, geom.AsCoordinates(array))
+	}
+	return geometry.CreatePolygon(jsonLine.Id, coords, jsonLine.Meta)
+}
+
+func getPointObjects(index int, feat *geojson.Feature) []JSONPoint {
+	var objs = make([]JSONPoint, 0, 1)
+	var meta, err = json.Marshal(feat.Properties)
+	checkError(err)
+	if feat.Geometry.IsPoint() {
+		var id = composeId(index, getFId(feat.Properties), 0)
+		objs = append(objs, JSONPoint{id, feat.Geometry.Point, string(meta)})
+	} else if feat.Geometry.IsMultiPoint() {
+		objs = make([]JSONPoint, 0, len(feat.Geometry.MultiPoint))
+		for pos, coords := range feat.Geometry.MultiPoint {
+			var id = composeId(index, getFId(feat.Properties), pos)
+			objs = append(objs, JSONPoint{id, coords, string(meta)})
+		}
+	}
+	return objs
+}
+
+func getLineStringObjects(index int, feat *geojson.Feature) []JSONLineString {
+	var objs = make([]JSONLineString, 0, 1)
+	var meta, err = json.Marshal(feat.Properties)
+	checkError(err)
+	if feat.Geometry.IsLineString() {
+		var id = composeId(index, getFId(feat.Properties), 0)
+		objs = append(objs, JSONLineString{id, feat.Geometry.LineString, string(meta)})
+	} else if feat.Geometry.IsMultiLineString() {
+		objs = make([]JSONLineString, 0, len(feat.Geometry.MultiLineString))
+		for pos, coords := range feat.Geometry.MultiLineString {
+			var id = composeId(index, getFId(feat.Properties), pos)
+			objs = append(objs, JSONLineString{id, coords, string(meta)})
+		}
+	}
+	return objs
+}
+
+func getPolygonObjects(index int, feat *geojson.Feature) []JSONPolygon {
+	var objs = make([]JSONPolygon, 0, 1)
+	var meta, err = json.Marshal(feat.Properties)
+	checkError(err)
+	if feat.Geometry.IsPolygon() {
+		var id = composeId(index, getFId(feat.Properties), 0)
+		objs = append(objs, JSONPolygon{id, feat.Geometry.Polygon, string(meta)})
+	} else if feat.Geometry.IsMultiPolygon() {
+		objs = make([]JSONPolygon, 0, len(feat.Geometry.MultiPolygon))
+		for pos, coords := range feat.Geometry.MultiPolygon {
+			var id = composeId(index, getFId(feat.Properties), pos)
+			objs = append(objs, JSONPolygon{id, coords, string(meta)})
+		}
+	}
+	return objs
+}
+
+func getFId(properties map[string]interface{}) string {
+	var id = properties["id"]
+	if id == nil {
+		return "?"
+	}
+	return fmt.Sprintf("%v", id)
+}
+
+func composeId(index int, fid string, pos int) string {
+	return fmt.Sprintf("idx:%v-fid:%v-pos:%v", index, fid, pos)
+}
+
+func readJsonFile(file string) []string {
 	var data, err = fileutil.ReadAllOfFile(file)
 	checkError(err)
-	var tokens  = strings.Split(strings.TrimSpace(data), "\n")
+	var tokens = strings.Split(strings.TrimSpace(data), "\n")
 	for i := range tokens {
 		tokens[i] = strings.TrimSpace(tokens[i])
 	}
-	parseFeatures(tokens)
+	return tokens
 }
 
 func checkError(err error) {
